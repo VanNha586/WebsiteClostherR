6726f16297f57ac9e6b03dc6eeeeff70a2d3939b
VanNha586 <82138603+VanNha586@users.noreply.github.com>
2022-06-05 19:55:27 +0700

    ...rest fake data

--- src/App.js
+++ src/App.js
@@ -1,12 +1,51 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 
-import HomePage from './pages/homepage/homepage.component';
+import "./App.css";
+// import HomePage from "./pages/homepage/homepage.component";
+
+import ShopPage from "./Shop/shop.component.jsx";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+};
+const HatsPage = () => {
+  return (
+    <div>
+      <h1>Hats Page</h1>
+    </div>
+  );
+};
+const TopicsList = () => {
+  return (
+    <div>
+      <h1>Topic List Page</h1>
+    </div>
+  );
+};
+const TopicsDetail = (props) => {
+  return (
+    <div>
+      <h1>Topic Detail Page {props.match.params.topicId}</h1>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route exact path="/hats" element={<HatsPage />} />
+        <Route exact path="/shop" element={<ShopPage />} />
+        <Route exact path="/topics" element={<TopicsList />} />
+        <Route path="/topics/:topicId" component={TopicsDetail} />
+      </Routes>
+      {/* <HomePage /> */}
     </div>
   );
 }
